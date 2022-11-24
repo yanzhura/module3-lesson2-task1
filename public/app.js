e704d2a0ebebc962a0124b7ffe5118745bf71742
Mikhail Yanzhura <yanzhura@gmail.com>
2022-11-24 15:03:07 +0300

    Промежуточный коммит

--- public/app.js
+++ public/app.js
@@ -1,5 +1,4 @@
-let listItemElement = '';
-let listItemHTML = '';
+const savedNotes = [];
 
 document.addEventListener('click', (event) => {
     if (event.target.dataset.type === 'remove') {
@@ -10,13 +9,26 @@ document.addEventListener('click', (event) => {
     } else if (event.target.dataset.type === 'edit') {
         const id = event.target.dataset.id;
         listItemElement = event.target.closest('li');
-        listItemHTML = listItemElement.innerHTML;
+        const listItemNode = listItemElement.childNodes[0];
+        console.log('listItemNode', listItemNode.innerText);
         listItemElement.innerHTML = getEditForm(id);
     } else if (event.target.dataset.type === 'submit') {
         const id = event.target.dataset.id;
-        const el = document.querySelector(`input[data-id="${id}"]`);
+        listItemElement = event.target.closest('li');
+        const inputElement = document.querySelector(`input[data-id="${id}"]`);
+        const title = inputElement.value;
+        if (newTitle) {
+            const newNote = {
+                id,
+                title
+            };
+            update(newNote).then(() => {
+                listItemElement.innerHTML = getNoteElement(newNote);
+            });
+        }
     } else if (event.target.dataset.type === 'cancel') {
-        listItemElement.innerHTML = listItemHTML;
+        const id = event.target.dataset.id;
+        console.log('id :>> ', id);
     }
 });
 
@@ -25,8 +37,21 @@ const getEditForm = (id) =>
 <input type="text" id="title" data-id="${id}"/>
 <div>
     <button class="btn btn-success" data-type="submit" data-id="${id}">Save</button>
-    <button class="btn btn-danger" data-type="cancel">Cancel</button>
+    <button class="btn btn-danger" data-type="cancel" data-id="${id}">Cancel</button>
 </div>`;
+
+const getNoteElement = ({ id, title }) =>
+    `
+${title}
+<div class="buttons">
+    <button class="btn btn-primary" data-type="edit" data-id="${id}">
+        Update
+    </button>
+    <button class="btn btn-danger" data-type="remove" data-id="${id}">
+        &times;
+    </button>
+</div>
+`;
 
 const remove = async (id) => {
     await fetch(`/${id}`, {
