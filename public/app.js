424b1743e75a6b81c2c79a82c7fc361e31be6d42
Mikhail Yanzhura <yanzhura@gmail.com>
2022-11-24 14:20:05 +0300

    Сделны кнопки и форма

--- public/app.js
+++ public/app.js
@@ -1,3 +1,6 @@
+let listItemElement = '';
+let listItemHTML = '';
+
 document.addEventListener('click', (event) => {
     if (event.target.dataset.type === 'remove') {
         const id = event.target.dataset.id;
@@ -5,19 +8,25 @@ document.addEventListener('click', (event) => {
             event.target.closest('li').remove();
         });
     } else if (event.target.dataset.type === 'edit') {
-        const newTitle = prompt('Введите новое название');
-        if (newTitle) {
-            const newNote = {
-                id: event.target.dataset.id,
-                title: newTitle
-            };
-            update(newNote).then(() => {
-                const node = event.target.closest('li').childNodes[0];
-                node.nodeValue = newTitle;
-            });
-        }
+        const id = event.target.dataset.id;
+        listItemElement = event.target.closest('li');
+        listItemHTML = listItemElement.innerHTML;
+        listItemElement.innerHTML = getEditForm(id);
+    } else if (event.target.dataset.type === 'submit') {
+        const id = event.target.dataset.id;
+        const el = document.querySelector(`input[data-id="${id}"]`);
+    } else if (event.target.dataset.type === 'cancel') {
+        listItemElement.innerHTML = listItemHTML;
     }
 });
+
+const getEditForm = (id) =>
+    `
+<input type="text" id="title" data-id="${id}"/>
+<div>
+    <button class="btn btn-success" data-type="submit" data-id="${id}">Save</button>
+    <button class="btn btn-danger" data-type="cancel">Cancel</button>
+</div>`;
 
 const remove = async (id) => {
     await fetch(`/${id}`, {
@@ -30,3 +39,19 @@ const update = async (payload) => {
         method: 'PUT'
     });
 };
+
+//! Оставлено для примера
+const oldEdit = (params) => {
+    const newTitle = prompt('Введите новое название');
+    if (newTitle) {
+        const newNote = {
+            id: event.target.dataset.id,
+            title: newTitle
+        };
+        update(newNote).then(() => {
+            const node = event.target.closest('li').childNodes[0];
+            node.nodeValue = newTitle;
+        });
+    }
+};
+//! Оставлено для примера
